10a5b1b737f26af2ece6bc0766e6816cf926a9c4
JB Ferguson <jbferguson85@gmail.com>
2024-03-20 07:16:56 -0500

    Add more fields to line struct.

--- backend/models/line.go
+++ backend/models/line.go
@@ -1,9 +1,15 @@
 package models
 
 type Line struct {
-	Id        string
-	Action    string
-	Name      string
-	ItemType  string
-	ItemValue string
+	Id           string
+	Action       string
+	Name         string
+	ItemType     string
+	ItemValue    string
+	ItemFunction string
+	ItemOwner    string
+	City         string
+	ItemCode     string
+	AddedDate    string
+	ItemSubType  string
 }
